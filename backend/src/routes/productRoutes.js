894cf7017fdb62d76cf398bb13c1a76a88587f90
Kennethd21 <jaushdsouza@gmail.com>
2025-11-22 16:12:38 +0530

    Remove unnecessary blank line in productRoutes.js

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { getAllProducts } from "../controllers/productController.js";
 
 const router = Router();
-
 router.get("/", getAllProducts);
 
 export default router;
