64ae60605e6b7f893cbadde253798497fbeb21a9
Kennethd21 <jaushdsouza@gmail.com>
2025-11-22 16:13:41 +0530

    Refactor database connection configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,13 @@
-import pkg from "pg";
+
+import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
